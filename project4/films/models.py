15d7d205584f7579b8b98af4a99d82f39901b5c7
alramama <74817864+alramama@users.noreply.github.com>
2022-08-09 15:04:40 +0300

    Add files via upload

---
+++ project4/films/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
+
+
+
+class Film(models.Model):
+    bid_description  = models.CharField(blank = True, max_length =255 )
+    End_user = models.CharField(blank = True, max_length =255 )
+    activity_type = models.CharField(blank = True, max_length =255 )
+    reference_no = models.CharField(blank = True, max_length =50 )
+    bid_file = models.FileField(upload_to='media',blank = True)
+    
+
